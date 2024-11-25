b937cb9cc5cb7482f6da789faff0cb1fbc43768f
Le Trung Hieu <le.trung.hieu@zensho.com>
2024-11-25 10:17:41 +0700

    add

--- app/src/main/java/com/example/multi_module/MainActivity.kt
+++ app/src/main/java/com/example/multi_module/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.multi_module.databinding.ActivityMainBinding
+import com.example.shared_resource.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
